9f0920819df16924322b55df7031f09cfe070725
ajaybansal072006 <ajaybansal072006@gmail.com>
2025-10-29 18:44:19 +0530

    Delete src directory

--- src/audio_to_text.py
+++
@@ -1,9 +0,0 @@
-import whisper
-
-def convert_audio_to_text(audio_path: str):
-    """
-    Transcribes user’s voice describing plant symptoms into text.
-    """
-    model = whisper.load_model("small")
-    result = model.transcribe(audio_path)
-    return result['text']
